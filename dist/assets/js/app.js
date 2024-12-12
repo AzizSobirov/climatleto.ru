31f587a2d1500cdd6140181dea7170c2cb256abf
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-12-12 16:51:19 +0500

    12.12.2024

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -90,6 +90,38 @@ if (header) {
   });
 }
 
+// ** Accordions ** //
+const getAccordionParents = document.querySelectorAll("[data-accordion");
+getAccordionParents.forEach((parent) => {
+  const isMultiple = parent.dataset.multiple;
+  const accordions = parent.querySelectorAll(".accordion");
+  accordions.forEach((accordion) => {
+    const header = accordion.querySelector(".accordion__header");
+    const body = accordion.querySelector(".accordion__body");
+    const content = accordion.querySelector(".accordion__content");
+
+    header.addEventListener("click", () => {
+      const isActive = accordion.classList.contains("active");
+      if (!isActive) {
+        accordion.classList.add("active");
+        body.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        accordion.classList.remove("active");
+        body.style.maxHeight = 0;
+      }
+
+      if (!isMultiple || isMultiple == "false") {
+        accordions.forEach((el) => {
+          if (el !== accordion) {
+            el.classList.remove("active");
+            el.querySelector(".accordion__body").style.maxHeight = 0;
+          }
+        });
+      }
+    });
+  });
+});
+
 // phone mask
 const phoneMasks = document.querySelectorAll("input[name='phone']");
 phoneMasks.forEach((input) => {
